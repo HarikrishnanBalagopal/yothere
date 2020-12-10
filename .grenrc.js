f42c55e224c885f8b8214c2df1ad4c57c6e2a8a6
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2020-12-10 16:15:47 +0530

    fix: template for creating release
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- .grenrc.js
+++ .grenrc.js
@@ -1,10 +1,27 @@
+let printPreamble = true;
+
+function templ({ labels, name, text, url }) {
+    const preamble = `# Changelog
+Some general stuff we want to say.
+`;
+    const line = `- ${name} [${text}](${url})`;
+    if (printPreamble) {
+        printPreamble = false;
+        return preamble + line;
+    }
+    return line;
+}
+
 module.exports = {
     "dataSource": "prs",
-    "prefix": "v",
+    "prefix": "Move2Kube v",
     "onlyMilestones": false,
     "groupBy":
     {
         "🚀 Features": ["enhancement"],
         "🐛 Bug Fixes": ["bug"]
+    },
+    "template": {
+        "issue": templ,
     }
 }

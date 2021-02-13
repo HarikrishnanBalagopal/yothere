886c12e4fdcce45e4e3daff052dd036c66d689a7
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2021-02-14 00:25:48 +0530

    fixup! fixup! sss
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- .grenrc.js
+++ .grenrc.js
@@ -1,12 +1,11 @@
 module.exports = {
     title_prefix: "[WIP] YoThere ",
     // valid PR types: ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']
-    sections:
-        [
-            { title: "🚀 Features", labels: ["enhancement", "feat", "perf"] },
-            { title: "🐛 Bug Fixes", labels: ["bug", "fix", "revert"] },
-            { title: "🧹 Maintenance", labels: ["docs", "style", "refactor", "test", "build", "ci", "chore"] },
-        ],
+    sections: [
+        { title: "🚀 Features", labels: ["enhancement", "feat", "perf"] },
+        { title: "🐛 Bug Fixes", labels: ["bug", "fix", "revert"] },
+        { title: "🧹 Maintenance", labels: ["docs", "style", "refactor", "test", "build", "ci", "chore"] },
+    ],
     header: `For more documentation and support please visit https://konveyor.io/move2kube/
 # Changelog`,
     line_template: x => `- yoyo ${x.title} [#${x.number}](${x.html_url})`,
